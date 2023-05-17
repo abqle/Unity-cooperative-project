7de7aca8c4a37cb81effa6c245127612670edab4
ssv22 <ssv22@ctt-adm.int>
2023-05-17 18:55:33 +0300

    Animated background
    White sound in main menu is now animated!

---
+++ Assets/Scripts/AnimatedBackground.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedBackground : MonoBehaviour
+{
+    Material material = null;
+
+    private void Awake()
+    {
+        var img = GetComponent<Image>();
+        img.material = new Material(img.material);
+        material = img.material;
+        material.name = material.name + " (CLONE)";
+        StartCoroutine(frequency());
+
+    }
+
+    void Update()
+    {
+    }
+
+    IEnumerator frequency()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.02f);
+            material.mainTextureOffset = new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+}
